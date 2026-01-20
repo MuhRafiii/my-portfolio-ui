79ca93b652788f41d41c6967e9d97094a290cf17
M Rafi <mrafi0603@gmail.com>
2026-01-20 15:17:03 +0700

    add persistor

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore } from "redux-persist";
 import userReducer from "./userSlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
